ba3156e4b5bc711974e451b9e8d6f530f40cbbdf
Naitik Verma <naitikv2311@gmail.com>
2025-07-03 10:13:36 +0530

    Revert "done"
    This reverts commit cad23b827979d1cdea7d620c244684196cbd1d1c.

--- main/GetStartedPage/src/Email.jsx
+++ main/GetStartedPage/src/Email.jsx
@@ -84,7 +84,8 @@ function Email({ showRating, compact, currentUser }) {
         <button type="submit" className="footer-submit-btn">SUBMIT</button>
         {submitted && <div className="thank-you">Thank you for your message!</div>}
       </form>
-      {showRating && <Rating simple={true} />}
+      {showRating && <Rating currentUser={{ email }} />}
+      <Rating simple={true} />
     </div>
   );
 }
